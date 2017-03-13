7d17e3ec018b85dac9a1f36c99023e609ad06d7a
richardverheyen <richard.verheyen@gmail.com>
2017-03-13 18:02:19 +1100

    Parallax and fade effect added to feature project 1

--- src/js/main.js
+++ src/js/main.js
@@ -5,52 +5,28 @@ $(document).ready(function() {
 
   logoAnimation();
 
-  //Animation effects for landing page feature projects section
-  $(window).scroll(function() {
-    var viewFromTop = $(window).scrollTop();
-    var viewHeight = $(window).height();
-    var elementActivationPoint = viewFromTop + (viewHeight * 2 / 3);
-
-    var introGoodLooking = $('#intro h2 > span').offset().top;
-    if (introGoodLooking < elementActivationPoint) {
-      $('#intro h2 > span').addClass('active');
-    };
-    var cosmeticAvenueImage = $('#ca-image').offset().top;
-    if (cosmeticAvenueImage < elementActivationPoint) {
-      $('#ca-image').addClass('active');
-    };
-    var jeepImage = $('#jeep-image').offset().top;
-    if (jeepImage < elementActivationPoint) {
-      $('#jeep-image').addClass('active');
-    };
-    var finsecImages = $('#finsec .images img').offset().top;
-    if (finsecImages < elementActivationPoint) {
-      $('#finsec .images img:nth-last-child(3)').addClass('active');
-      $('#finsec .images img:nth-last-child(2)').addClass('active');
-      $('#finsec .images img:nth-last-child(1)').addClass('active');
-    };
-    var karenWilliamsCard = $('#sliding-cards .feature-card').offset().top;
-    if (karenWilliamsCard < elementActivationPoint) {
-      $('#sliding-cards .feature-card').addClass('active');
-    };
-    var karenWilliamsCardMobile = $('#sliding-cards .feature-card-mobile').offset().top;
-    if (karenWilliamsCardMobile < elementActivationPoint) {
-      $('#sliding-cards .feature-card-mobile').addClass('active');
-    };
-
-  });
+  var viewHeight = $(window).height();
+  var $cosmeticAvenueTop = $('#cosmetic-avenue').offset().top;
+  var $cosmeticAvenueHeight = $('#cosmetic-avenue').height();
 
   $(window).scroll(function() {
     var viewFromTop = $(window).scrollTop();
-    var viewHeight = $(window).height();
-    var elementHeight = $('#cosmetic-avenue').height();
-    var fadeActivationPoint = viewFromTop + (viewHeight * 4 / 5);
-    var cosmeticAvenueImage2 = $('#cosmetic-avenue').offset().top;
+    var parallaxBackgroundHeight = $cosmeticAvenueHeight * 6 / 5;
 
-    // $opacity = ((viewFromTop + viewHeight) - cosmeticAvenueImage2) * -1 / (elementHeight);
-    console.log(viewFromTop + (viewHeight * 4 / 5));
-    console.log(cosmeticAvenueImage2);
-    // $('body').css('opacity', $opacity);
+    $('#cosmetic-avenue .background').css('top', -(viewFromTop - $cosmeticAvenueTop) / 5 - (parallaxBackgroundHeight / 12) - // sliding down;
+      $cosmeticAvenueHeight / 2 +
+      ('px')
+    );
+    // set 0 point using a function of everything but viewHeight
+    var zeroPoint = $cosmeticAvenueTop + (viewHeight / 4);
+
+    // Use viewHeight to vary the parallax around the 0 point
+    var $scrollPosition = viewFromTop + viewHeight;
+    // scale the movement relative to screen size using the correct scaling factor so it falls within 0 and 1
+    var $scaling = 10 / (4 * viewHeight);
+    $opacity = (zeroPoint - $scrollPosition) * $scaling * -1;
+    $('#cosmetic-avenue').css('opacity', $opacity);
+
   });
 
   //open side menu on header click
@@ -116,5 +92,40 @@ $(document).ready(function() {
     }, 1500);
 
   };
+
+  //Animation effects for landing page feature projects section
+  $(window).scroll(function() {
+    var viewFromTop = $(window).scrollTop();
+    var viewHeight = $(window).height();
+    var elementActivationPoint = viewFromTop + (viewHeight * 2 / 3);
+
+    var introGoodLooking = $('#intro h2 > span').offset().top;
+    if (introGoodLooking < elementActivationPoint) {
+      $('#intro h2 > span').addClass('active');
+    };
+    var cosmeticAvenueImage = $('#ca-image').offset().top;
+    if (cosmeticAvenueImage < elementActivationPoint) {
+      $('#ca-image').addClass('active');
+    };
+    var jeepImage = $('#jeep-image').offset().top;
+    if (jeepImage < elementActivationPoint) {
+      $('#jeep-image').addClass('active');
+    };
+    var finsecImages = $('#finsec .images img').offset().top;
+    if (finsecImages < elementActivationPoint) {
+      $('#finsec .images img:nth-last-child(3)').addClass('active');
+      $('#finsec .images img:nth-last-child(2)').addClass('active');
+      $('#finsec .images img:nth-last-child(1)').addClass('active');
+    };
+    var karenWilliamsCard = $('#sliding-cards .feature-card').offset().top;
+    if (karenWilliamsCard < elementActivationPoint) {
+      $('#sliding-cards .feature-card').addClass('active');
+    };
+    var karenWilliamsCardMobile = $('#sliding-cards .feature-card-mobile').offset().top;
+    if (karenWilliamsCardMobile < elementActivationPoint) {
+      $('#sliding-cards .feature-card-mobile').addClass('active');
+    };
+
+  });
 
 });
