79a9cf2c4c6dd912d059bdaa48224cbd25aad8b1
richardverheyen <richard.verheyen@gmail.com>
2017-03-15 10:38:04 +1100

    Parallax updates for mobile on train

--- src/js/main.js
+++ src/js/main.js
@@ -5,30 +5,6 @@ $(document).ready(function() {
 
   logoAnimation();
 
-  var viewHeight = $(window).height();
-  var $cosmeticAvenueTop = $('#cosmetic-avenue').offset().top;
-  var $cosmeticAvenueHeight = $('#cosmetic-avenue').height();
-
-  $(window).scroll(function() {
-    var viewFromTop = $(window).scrollTop();
-    var parallaxBackgroundHeight = $cosmeticAvenueHeight * 6 / 5;
-
-    $('#cosmetic-avenue .background').css('top', -(viewFromTop - $cosmeticAvenueTop) / 5 - (parallaxBackgroundHeight / 12) - // sliding down;
-      $cosmeticAvenueHeight / 2 +
-      ('px')
-    );
-    // set 0 point using a function of everything but viewHeight
-    var zeroPoint = $cosmeticAvenueTop + (viewHeight / 4);
-
-    // Use viewHeight to vary the parallax around the 0 point
-    var $scrollPosition = viewFromTop + viewHeight;
-    // scale the movement relative to screen size using the correct scaling factor so it falls within 0 and 1
-    var $scaling = 10 / (4 * viewHeight);
-    $opacity = (zeroPoint - $scrollPosition) * $scaling * -1;
-    $('#cosmetic-avenue').css('opacity', $opacity);
-
-  });
-
   //open side menu on header click
   $('#menu-link').on('click', function() {
     if (showingMenu) {
@@ -95,8 +71,8 @@ $(document).ready(function() {
 
   //Animation effects for landing page feature projects section
   $(window).scroll(function() {
-    var viewFromTop = $(window).scrollTop();
     var viewHeight = $(window).height();
+    var viewFromTop = $(window).scrollTop();
     var elementActivationPoint = viewFromTop + (viewHeight * 2 / 3);
 
     var introGoodLooking = $('#intro h2 > span').offset().top;
@@ -126,6 +102,60 @@ $(document).ready(function() {
       $('#sliding-cards .feature-card-mobile').addClass('active');
     };
 
+  });
+
+  $('#services #illustration .parallax img').each(function(index) {
+    var $id = index;
+    var $offset = Number($(this).offset());
+    var $yPosOriginal = Number($(this).css('top').replace('px', ''));
+    var $zPos = Number($(this).parent().parent().attr('data'));
+
+  });
+
+  $(window).scroll(function() {
+    var $cosmeticAvenueTop = $('#cosmetic-avenue').offset().top;
+    var $cosmeticAvenueHeight = $('#cosmetic-avenue').height();
+    var viewHeight = $(window).height();
+    var viewFromTop = $(window).scrollTop();
+    var parallaxBackgroundHeight = $cosmeticAvenueHeight * 11 / 5;
+
+    $('#cosmetic-avenue .background').css('top', -(viewFromTop - $cosmeticAvenueTop) / 5 - (parallaxBackgroundHeight / 12) - // sliding down;
+      $cosmeticAvenueHeight / 2 +
+      ('px')
+    );
+    // set 0 point using a function of everything but viewHeight
+    var zeroPoint = $cosmeticAvenueTop + (viewHeight / 4);
+
+    // Use viewHeight to vary the parallax around the 0 point
+    var $scrollPosition = viewFromTop + viewHeight;
+    // scale the movement relative to screen size using the correct scaling factor so it falls within 0 and 1
+    var $scaling = 10 / (4 * viewHeight);
+    $opacity = (zeroPoint - $scrollPosition) * $scaling * -1;
+    $('#cosmetic-avenue').css('opacity', $opacity);
+    $('#cosmetic-avenue .background').css('opacity', $opacity);
+  });
+
+  $(window).scroll(function() {
+    var $jeepTop = $('#jeep').offset().top;
+    var $jeepHeight = $('#jeep').height();
+    var viewHeight = $(window).height();
+    var viewFromTop = $(window).scrollTop();
+    var parallaxBackgroundHeight = $jeepHeight * 6 / 5;
+
+    $('#jeep .background').css('top', -(viewFromTop - $jeepTop) / 5 - (parallaxBackgroundHeight / 12) - // sliding down;
+      $jeepHeight / 2 +
+      ('px')
+    );
+    // set 0 point using a function of everything but viewHeight
+    var zeroPoint = $jeepTop + (viewHeight / 4);
+
+    // Use viewHeight to vary the parallax around the 0 point
+    var $scrollPosition = viewFromTop + viewHeight;
+    // scale the movement relative to screen size using the correct scaling factor so it falls within 0 and 1
+    var $scaling = 10 / (4 * viewHeight);
+    $opacity = (zeroPoint - $scrollPosition) * $scaling * -1;
+    $('#jeep').css('opacity', $opacity);
+    $('#jeep .background').css('opacity', $opacity);
   });
 
 });
