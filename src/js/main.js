69b7fd71ed10b7bc3e1f02756ee77facffbb97e5
richardverheyen <richard.verheyen@gmail.com>
2017-04-03 20:53:46 +1000

    Move instafeed from the layout page to the main.js file

--- src/js/main.js
+++ src/js/main.js
@@ -166,6 +166,16 @@ $(document).ready(function() {
       };
 
     });
+
+    // instafeed
+    var feed = new Instafeed({
+      get: 'user',
+      userId: '1437536913',
+      accessToken: '1437536913.1677ed0.02677d6ce703465e80498fcd44f92c54',
+      limit: 10,
+      resolution: 'standard_resolution'
+    });
+    feed.run();
   };
 
   if ($('body').is('#services')) {
