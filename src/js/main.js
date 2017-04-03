4175644c592ac31e80aeb2296118cafc24d019b5
richardverheyen <richard.verheyen@gmail.com>
2017-04-03 14:05:31 +1000

    Add google analytics

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,26 @@
+//Google Analytics start
+/* jshint ignore:start */
+
+(function(i, s, o, g, r, a, m) {
+  i['GoogleAnalyticsObject'] = r;
+  i[r] = i[r] || function() {
+    (i[r].q = i[r].q || []).push(arguments)
+  }, i[r].l = 1 * new Date();
+  a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+/* jshint ignore:end */
+if (!!ga) {
+  ga('create', 'UA-85086943-1', 'auto');
+  ga('set', {
+    dimension1: environment
+  });
+  ga('send', 'pageview');
+}
+//Google Analytics end
+
 var showingMenu = false;
 var showingLogo = true;
 
